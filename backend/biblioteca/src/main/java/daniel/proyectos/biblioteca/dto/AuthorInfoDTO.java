c3c7993566a41149d3dedccc629cfccd69dbd810
Daniel Hdez <BQDANNYHDEZ@GMAIL.COM>
2024-11-15 14:35:28 +0000

    #11
    Create AuthorDTO and Mapper

---
+++ backend/biblioteca/src/main/java/daniel/proyectos/biblioteca/dto/AuthorInfoDTO.java
@@ -0,0 +1,10 @@
+package daniel.proyectos.biblioteca.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthorInfoDTO {
+    private int id;
+    private String name;
+    private String surnames;
+}

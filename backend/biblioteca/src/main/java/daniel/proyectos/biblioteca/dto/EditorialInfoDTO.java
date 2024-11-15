cbfb5e1df52be9ecba0b3370027939a15f8e019c
Daniel Hdez <BQDANNYHDEZ@GMAIL.COM>
2024-11-15 14:33:02 +0000

    #12
    Create EditoriaDTO and Mapper

---
+++ backend/biblioteca/src/main/java/daniel/proyectos/biblioteca/dto/EditorialInfoDTO.java
@@ -0,0 +1,10 @@
+package daniel.proyectos.biblioteca.dto;
+
+import lombok.Data;
+
+@Data
+public class EditorialInfoDTO  {
+    private int id;
+    private String name;
+    private String logo;
+}

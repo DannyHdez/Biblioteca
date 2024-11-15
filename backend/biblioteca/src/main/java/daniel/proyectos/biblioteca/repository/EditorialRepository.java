994de60124118599ce0ee6208cdac6f3dae9819b
Daniel Hdez <BQDANNYHDEZ@GMAIL.COM>
2024-11-15 15:01:40 +0000

    #76

---
+++ backend/biblioteca/src/main/java/daniel/proyectos/biblioteca/repository/EditorialRepository.java
@@ -0,0 +1,7 @@
+package daniel.proyectos.biblioteca.repository;
+
+import daniel.proyectos.biblioteca.entity.Editorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EditorialRepository extends JpaRepository<Editorial, Integer> {
+}

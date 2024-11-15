853d06731a76039e5e2bc65096b5fc110bb3deaf
Daniel Hdez <BQDANNYHDEZ@GMAIL.COM>
2024-11-15 15:02:02 +0000

    #78

---
+++ backend/biblioteca/src/main/java/daniel/proyectos/biblioteca/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package daniel.proyectos.biblioteca.repository;
+
+import daniel.proyectos.biblioteca.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+}

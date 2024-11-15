2f3467050f5ff2e11c5f20b28a3c8026d1bfb718
Daniel Hdez <BQDANNYHDEZ@GMAIL.COM>
2024-11-15 14:24:41 +0000

    #8
    Create genre entity

---
+++ backend/biblioteca/src/main/java/daniel/proyectos/biblioteca/entity/Genre.java
@@ -0,0 +1,25 @@
+package daniel.proyectos.biblioteca.entity;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "genres")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @JsonIgnoreProperties("genres")
+    @ManyToMany(mappedBy = "genres")
+    private List<Book> books;
+
+    // Getters y setters
+}

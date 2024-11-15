8d41363cffaa2f6dcf865b79a225506ea7dbe3cf
Daniel Hdez <BQDANNYHDEZ@GMAIL.COM>
2024-11-15 15:01:29 +0000

    #75

---
+++ backend/biblioteca/src/main/java/daniel/proyectos/biblioteca/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package daniel.proyectos.biblioteca.repository;
+
+import daniel.proyectos.biblioteca.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    List<Book> findByUserId(int userId);
+}
